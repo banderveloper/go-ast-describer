eae1ca4ebb68eef02428572a104d607a424666a6
banderveloper <kpssdeveloper@gmail.com>
2024-07-21 22:26:40 +0300

    feat: add comments methods for StructMethodModel

--- model/struct_method_model.go
+++ model/struct_method_model.go
@@ -1,5 +1,7 @@
 package model
 
+import "strings"
+
 // description of structure method
 type StructMethodModel struct {
 	Name       string // method name
@@ -13,4 +15,54 @@ type StructMethodArg struct {
 	Index int
 	Name  string
 	Type  string
+}
+
+// get all structure comments with given prefix, including it
+func (sm *StructMethodModel) GetCommentsWithPrefix(prefix string) []string {
+
+	if prefix == "" {
+		return sm.Comments
+	}
+
+	resultComments := make([]string, 0)
+
+	for _, comment := range sm.Comments {
+		if strings.HasPrefix(comment, prefix) {
+			resultComments = append(resultComments, comment)
+		}
+	}
+
+	return resultComments
+}
+
+// whether structure has comment with prefix
+func (sm *StructMethodModel) HasCommentWithPrefix(prefix string) bool {
+
+	if prefix == "" && len(sm.Comments) > 0 {
+		return true
+	}
+
+	for _, comment := range sm.Comments {
+		if strings.HasPrefix(comment, prefix) {
+			return true
+		}
+	}
+
+	return false
+}
+
+// whether structure contains given comment
+func (sm *StructMethodModel) HasComment(comment string) bool {
+
+	if len(sm.Comments) == 0 {
+		return false
+	}
+
+	for _, structComment := range sm.Comments {
+		if structComment == comment {
+			return true
+		}
+	}
+
+	return false
 }
