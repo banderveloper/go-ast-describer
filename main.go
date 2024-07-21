5aad1aa61bcb23f1fa09ecfa112316d5b9305703
banderveloper <kpssdeveloper@gmail.com>
2024-07-21 15:08:02 +0300

    feat: add getting field struct tags map

--- main.go
+++ main.go
@@ -1,8 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/banderveloper/go-ast-describer/model"
+)
 
 func main() {
 
-	fmt.Println("Hello world")
+	// fmt.Println("Hello world")
+
+	m := model.StructFieldModel{
+		Name:      "Name",
+		Type:      "string",
+		StructTag: `json:"hello" database:"idk" emma:"what"`,
+	}
+
+	tags := m.GetTags()
+	fmt.Println(tags["database"] == "idk")
+
 }
--- model/struct_field_model.go
+++ model/struct_field_model.go
@@ -1,5 +1,7 @@
 package model
 
+import "strings"
+
 // description of field of structure
 type StructFieldModel struct {
 	Name      string // field name
@@ -7,8 +9,30 @@ type StructFieldModel struct {
 	StructTag string // field struct tag without ``
 }
 
-// description of field's struct tag KV
-type StructTagKV struct {
-	Key   string
-	Value string
+// get field's struct tags as KV map
+func (sfm *StructFieldModel) GetTags() map[string]string {
+
+	result := make(map[string]string, 0)
+
+	// if field has no tag - result empty map
+	if sfm.StructTag == "" {
+		return result
+	}
+
+	// split tag by parts if case if there are a few keys
+	parts := strings.Split(sfm.StructTag, " ")
+
+	for _, part := range parts {
+		keyValue := strings.SplitN(part, ":", 2)
+		if len(keyValue) == 2 {
+			key := keyValue[0]
+			value := strings.Trim(keyValue[1], `"`)
+			result[key] = value
+		} else if len(keyValue) == 1 {
+			// Handle tags without values (e.g., `key`)
+			result[keyValue[0]] = ""
+		}
+	}
+
+	return result
 }
